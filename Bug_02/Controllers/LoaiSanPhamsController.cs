1592c473e1b268851c8e73585a0c0840d9d31cca
chikaturin <ninhhoaithanh@gmail.com>
2024-01-28 11:01:52 +0700

    first

---
+++ Bug_02/Controllers/LoaiSanPhamsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bug_02.Models;
+
+namespace Bug_02.Controllers
+{
+    public class LoaiSanPhamsController : Controller
+    {
+        private WebTh1Entities1 db = new WebTh1Entities1();
+
+        // GET: LoaiSanPhams
+        public ActionResult Index()
+        {
+            return View(db.LoaiSanPham.ToList());
+        }
+
+        // GET: LoaiSanPhams/Details/5
+        public ActionResult Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoaiSanPham loaiSanPham = db.LoaiSanPham.Find(id);
+            if (loaiSanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loaiSanPham);
+        }
+
+        // GET: LoaiSanPhams/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoaiSanPhams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCate,NameCate")] LoaiSanPham loaiSanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LoaiSanPham.Add(loaiSanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(loaiSanPham);
+        }
+
+        // GET: LoaiSanPhams/Edit/5
+        public ActionResult Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoaiSanPham loaiSanPham = db.LoaiSanPham.Find(id);
+            if (loaiSanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loaiSanPham);
+        }
+
+        // POST: LoaiSanPhams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCate,NameCate")] LoaiSanPham loaiSanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(loaiSanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(loaiSanPham);
+        }
+
+        // GET: LoaiSanPhams/Delete/5
+        public ActionResult Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoaiSanPham loaiSanPham = db.LoaiSanPham.Find(id);
+            if (loaiSanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loaiSanPham);
+        }
+
+        // POST: LoaiSanPhams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            LoaiSanPham loaiSanPham = db.LoaiSanPham.Find(id);
+            db.LoaiSanPham.Remove(loaiSanPham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
